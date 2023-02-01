31dd2bd13e69b445aa26949cc57bbad8ba253371
derivaldobrito <derivaldo.brito@yahoo.com.br>
2023-01-31 22:48:06 -0300

    Interface Multiples inheritage

---
+++ src/application/App.java
@@ -0,0 +1,24 @@
+package application;
+
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
+
+public class App {
+    public static void main(String[] args) {
+
+        ConcretePrinter p = new ConcretePrinter("1080");
+        p.processDoc("My Letter");
+        p.print("My Letter");
+
+        ConcreteScanner s = new ConcreteScanner("2003");
+        s.processDoc("My Email");
+        System.out.println("Scan result: " + s.scan());
+
+        ComboDevice c = new ComboDevice("2081");
+        c.processDoc("My dissertation");
+        c.print("My dissertation");
+        System.out.println("Scan result: " + c.scan());
+
+    }
+}
---
+++ src/devices/Printer.java
@@ -0,0 +1,9 @@
+package devices;
+
+public interface Printer {
+
+    void print(String doc);
+
+
+
+}
